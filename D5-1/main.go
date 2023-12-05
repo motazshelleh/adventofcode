35296cdf6d6bcebf3f373529b48af33656c10efd
motazshelleh <motazshelleh@gmail.com>
2023-12-05 23:40:56 +0200

    Clean up

--- D5-1/main.go
+++ D5-1/main.go
@@ -54,18 +54,12 @@ func main() {
 
 	src := seeds
 	for _, rm := range rangeMaps {
-		// for _, v := range rm {
-		// 	fmt.Println(v)
-		// }
 		sort.Slice(src, func(i, j int) bool {
 			return src[i] < src[j]
 		})
 		src = mapFromSourceToDest(rm, src)
-		// fmt.Println("src", src)
-		// break
 	}
-	fmt.Println("locations: ", src)
-	// min
+	fmt.Println("locations:", src)
 	min := src[0]
 	for _, v := range src {
 		if v < min {
@@ -92,22 +86,10 @@ func sortRangeMap(rm []rangeMap) {
 
 func mapFromSourceToDest(rm []rangeMap, input []int64) []int64 {
 	output := make([]int64, len(input))
-	fmt.Println("input", input)
-	// fmt.Println("rm", rm)
-	// rangeMapIndex := 0
 	for i := 0; i < len(input); i++ {
-		// fmt.Println(rangeMapIndex)
-
-		// for rangeMapIndex < len(rm) && input[i] > rm[rangeMapIndex].src+rm[rangeMapIndex].length-1 {
-		// 	rangeMapIndex++
-		// }
-
-		// fmt.Println("rangeMapIndex", rangeMapIndex, "input[i]", input[i])
-		// if rangeMapIndex < len(rm) && input[i] >= rm[rangeMapIndex].src && input[i] <= rm[rangeMapIndex].src+rm[rangeMapIndex].length-1 {
 		ok, r := validate(rm, input[i])
 		if ok {
 			mappedValue := input[i] - r.src + r.dest
-			// fmt.Println("mappedValue", mappedValue)
 			output[i] = mappedValue
 		} else {
 			output[i] = input[i]
